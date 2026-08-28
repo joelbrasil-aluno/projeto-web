ce5d6627289caa16cf1c770850a9012a32f81647
Joel Aparecido Brasil <joel.brasil@aluno.cps.sp.gov.br>
2026-08-27 21:55:50 -0300

    novos atualizações

---
+++ app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def pagina_inicial():
+    return '<h1>Olá</h1>'
+
+@app.route('/nome')
+def nome():
+    return '<p>Joel Brasil<p>'
+
+
+if __name__ == '__main__'
+    app.run(debug=true)
