889c4d9cbf092d58824d125c730715a7d66c4958
Joel Aparecido Brasil <joel.brasil@aluno.cps.sp.gov.br>
2026-09-03 21:59:45 -0300

    aula 03

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -10,6 +11,25 @@ def pagina_inicial():
 @app.route('/nome')
 def nome():
     return '<p>Joel Brasil<p>'
+
+@app.route('/html')
+def pagina_html():
+    return render_template('index.html')
+
+@app.route('/calcular/<nome>/<int:ano>')
+def calcular(nome,ano):
+    ano_atual = datetime.now().year
+    idade = ano_atual - ano
+
+    if idade > 18:
+        status = 'Maior de Idade'
+    else:
+        status = 'Menor de idade - ACESSO NEGADO!'
+
+    return render_template('variaveis.html', nome_usuario = nome,
+                                            ano_atual = ano_atual, nascimento = ano, 
+                                            idade = idade, status = status)
+
 
 
 if __name__ == '__main__':
